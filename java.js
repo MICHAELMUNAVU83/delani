3ba87a17bd021ade1c0a1d972c54e85051e41d8d
trance <michaelmunavu83@gmail.com>
2021-07-25 14:56:07 +0300

    started with javascript

---
+++ java.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('#des').hide()
+
+    $('#dess').click(function(){
+        $('#dess').hide()
+    })
+
+    $('#dess').click(function(){
+        $('#des').show()
+    })
+    $('#des').click(function(){
+        $('#des').hide()
+    })
+    $('#des').click(function(){
+        $('#dess').show()
+    })
+    
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+})
