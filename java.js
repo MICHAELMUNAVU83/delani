abed3e0020935eea436a6ad70d4c3b2aa3469c66
trance <michaelmunavu83@gmail.com>
2021-07-25 14:59:39 +0300

    finished desisn

--- java.js
+++ java.js
@@ -1,24 +1,40 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('#des').hide()
 
-    $('#dess').click(function(){
+    $('#dess').click(function () {
         $('#dess').hide()
     })
 
-    $('#dess').click(function(){
+    $('#dess').click(function () {
         $('#des').show()
     })
-    $('#des').click(function(){
+    $('#des').click(function () {
         $('#des').hide()
     })
-    $('#des').click(function(){
+    $('#des').click(function () {
         $('#dess').show()
     })
-    
+
+    $('#dev').hide()
+
+    $('#devv').click(function () {
+        $('#devv').hide()
+    })
+
+    $('#devv').click(function () {
+        $('#dev').show()
+    })
+    $('#dev').click(function () {
+        $('#dev').hide()
+    })
+    $('#dev').click(function () {
+        $('#devv').show()
+    })
 
 
-    
-    
+
+
+
 
 
 
